5ac50edc280d9ce92afc69a046f9560c9da070a2
Nat Welch <nat@natwelch.com>
2022-02-20 02:35:22 +0000

    Add timezone

--- main.go
+++ main.go
@@ -3,8 +3,36 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println(time.Now().Format(time.RFC3339))
+	app := &cli.App{
+		Name:  "isodate",
+		Usage: "Show the current time in RFC3339",
+		Flags: []Flag{
+			&StringFlag{
+				Name:    "time_zone",
+				Aliases: []string{"tz"},
+			},
+		},
+		Action: func(c *cli.Context) error {
+			now := time.Now()
+			tzString := c.String("time_zone")
+			if tzString != "" {
+				tz, err := time.LoadLocation("America/New_York")
+				if err != nil {
+					return err
+				}
+				now = now.In(tz)
+			}
+
+			fmt.Println(now.Format(time.RFC3339))
+			return nil
+		},
+		EnableBashCompletion: true,
+	}
+
+	app.Run(os.Args)
 }
